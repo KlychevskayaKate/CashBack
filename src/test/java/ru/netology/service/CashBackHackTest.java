48926d0d22572bde811ffbbd52fa3cabc6d8bca0
Klychevskaya Kate <kateklych@gmail.com>
2022-09-29 14:09:45 +0300

    add code testng

---
+++ src/test/java/ru/netology/service/CashBackHackTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashBackHackTest {
+    @Test
+    public void test1 (){
+        CashBackHackService service = new CashBackHackService();
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void test2 (){
+        CashBackHackService service = new CashBackHackService();
+        int amount = 1500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void test3 (){
+        CashBackHackService service = new CashBackHackService();
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void test4 (){
+        CashBackHackService service = new CashBackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals (actual, expected);
+    }
+}
